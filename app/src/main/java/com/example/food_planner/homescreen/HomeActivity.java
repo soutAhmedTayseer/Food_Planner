5c8dbe4efcc05873646ceef580e186946324a89e
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2026-01-26 03:41:09 +0200

    setting up initial home activity

--- app/src/main/java/com/example/food_planner/homescreen/HomeActivity.java
+++ app/src/main/java/com/example/food_planner/homescreen/HomeActivity.java
@@ -2,20 +2,29 @@ package com.example.food_planner.homescreen;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.food_planner.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        // TODO: Setup NavigationController here in the next step
+        // Best Practice: Connect NavController to BottomNav
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        }
     }
 }
