012f0b653be6e0bd3d79614fecbd7b1037480ad5
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2026-01-28 19:15:30 +0200

    retrofit and javarx

---
+++ app/src/main/java/com/example/food_planner/network/NetworkClient.java
@@ -0,0 +1,21 @@
+package com.example.food_planner.network;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
