46d891b4984850df2d3ec39cadc1b7384384523f
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2026-02-06 19:19:21 +0200

    alert for guest

--- app/src/main/java/com/example/food_planner/utils/AlertUtil.java
+++ app/src/main/java/com/example/food_planner/utils/AlertUtil.java
@@ -1,6 +1,8 @@
 package com.example.food_planner.utils;
 
 import android.content.Context;
+
+import com.example.food_planner.R;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class AlertUtil {
@@ -10,14 +12,21 @@ public class AlertUtil {
     }
 
     public static void showConfirmationDialog(Context context, String title, String message, OnPositiveClickListener listener) {
-        new MaterialAlertDialogBuilder(context)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("Yes", (dialog, which) -> {
-                    listener.onPositiveClick();
+        new MaterialAlertDialogBuilder(context).setTitle(title).setMessage(message).setPositiveButton(R.string.yes, (dialog, which) -> {
+            listener.onPositiveClick();
+            dialog.dismiss();
+        }).setNegativeButton(R.string.cancel, (dialog, which) -> dialog.dismiss()).show();
+    }
+
+    public static void showLoginRequiredDialog(Context context, OnPositiveClickListener onLoginClicked) {
+        new MaterialAlertDialogBuilder(context).setTitle(R.string.login_required)
+                .setMessage(R.string.you_need_to_sign_in_to_access_this_feature)
+                .setCancelable(false)
+                .setPositiveButton(R.string.login, (dialog, which) -> {
+                    onLoginClicked.onPositiveClick();
                     dialog.dismiss();
-                })
-                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
-                .show();
+                }).setNegativeButton(R.string.cancel, (dialog, which) -> {
+                    dialog.dismiss();
+                }).show();
     }
 }
