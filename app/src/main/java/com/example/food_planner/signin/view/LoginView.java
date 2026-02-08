1fc536fc8efb10b058fa778b738fde4562c56c20
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2026-02-08 23:19:34 +0200

    applying view structure

---
+++ app/src/main/java/com/example/food_planner/signin/view/LoginView.java
@@ -0,0 +1,13 @@
+package com.example.food_planner.signin.view;
+
+import android.content.Intent;
+
+public interface LoginView {
+    void showLoading();
+    void hideLoading();
+    void showSuccess(String message);
+    void showError(String message);
+    void navigateToHome();
+    void navigateToSignUp();
+    void launchGoogleSignIn(Intent signInIntent);
+}
